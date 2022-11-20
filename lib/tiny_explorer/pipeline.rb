bf7dc86c72b7d9675ed1c93008944aed20df463e
jefflunt <jefflunt@gmail.com>
2022-11-20 00:48:43 -0600

    seems a little bit like I'm reimplementing the less command here ... hm

--- lib/tiny_explorer/pipeline.rb
+++ lib/tiny_explorer/pipeline.rb
@@ -2,38 +2,53 @@ module TinyExplorer
   # this class implements a data pipeline through which log lines are passed,
   # resulting in output lines for display
   class Pipeline
+    attr_reader :read
+
     # io: the IO object from which to read log lines
-    # steps: the list of procs to be run over the IO log lines, in the order in
+    # procs: the list of procs to be run over the IO log lines, in the order in
     #   which they should be run
-    def initialize(io, steps)
-      @steps = steps
+    def initialize(io, procs)
+      @procs = procs
       @selected = []
+      @read = 0
 
-      Thread.new do
-        loop do
-          new_line = io.gets&.strip
-          break if new_line.nil?
+      puts "IO class: #{io.class}"
+      case io
+      when String
+        puts "Reading as String"
+        IO.read(io).lines.map(&:strip).each{|line| _process(line) }
+      when IO
+        puts "Reading as IO"
+        puts io.inspect
+        Thread.new do
+          loop do
+            line = io.gets&.strip
+            break if io.eof?
 
-          @selected << new_line
+            _process(line)
+          end
         end
       end
     end
 
+    # tells you the number of lines currently stored in this Pipeline
+    def length
+      @selected.length
+    end
+
+    # gives Array-like indexing over the lines in this Pipeline
     def [](window)
       @selected[window]
     end
 
-    def new_line(l)
-      return unless l
-
-      new_l = l
-
-      steps.each do |s|
-        new_l = s.call(new_l)
-        return if new_l.nil?
+    def _process(line)
+      @procs.each do |p|
+        break if line.nil?
+        line = p.call(line)
       end
 
-      @selected << new_l
+      @read += 1
+      @selected << line if line
     end
   end
 end
