24a869670605702641fa9eefd521bd534e6a66e4
jefflunt <jefflunt@gmail.com>
2025-03-26 10:24:10 -0500

    tests for config parsing and command handling

--- lib/config.rb
+++ lib/config.rb
@@ -4,23 +4,52 @@ require 'yaml'
 # this class reads the config.yml file (passed in as string), and generates the lens config
 class Config
   def self.parse(config_str)
-    Config.new(
+    Config.new(YAML.load(config_str))
   end
 
+  # config: a Hash containing a config, e.g. parsed from the ./config.yml file
   def initialize(config)
     @config = config
   end
 
-  # given the specified, current mode and keystroke, return the command to
-  # execute
+  # supports three kinds of commands:
   #
-  # for example, if you're in `cmd` mode and you press the 'C' key (defaults to
-  # "clear_to_eol"), then the `clear_to_eol` command is sent to the current
-  # buffer to clear from teh cursor to the end of the line.
+  # DIRECT: command is invoked immediately from the input keystroke
+  #   ex: 'd' to delete the current line from the 'home' mode
   #
-  # if a given keystroke doesn't result in a command, but starts a sub-command,
-  # then a timer is set for the sub-command to be selected. 
+  # subcmd: keystroke triggers one of several possible direct sub-commands
+  #   ex: 'b', then 'a' -> 'b' finds other 'b' commands having to do with buffer
+  #     management, and 'a' starts the `ms_buff_adj` command
+  #
+  # wildcard: keystroke readies a command, but waits for the followup
+  #   keystroke to invoke the command
+  #   ex: 'm', then 'c' -> 'm' readies the 'mark_set' command, but the input
+  #     waits for follow-up keystroke to set the name of the mark
   def cmd(mode, subcmd, keystroke)
-    
+    if subcmd
+      # handle wildcard
+      wildcard = _mode(mode)["#{subcmd}*"]
+      return "#{wildcard} #{keystroke}" if wildcard                           # wildcard
+
+      _mode(mode)["#{subcmd}#{keystroke}"]                                    # subcmd
+    else
+      return _mode(mode)[keystroke] if _mode(mode).keys.include?(keystroke)   # direct
+
+      subcmds = _mode(mode).keys.select{|k| k.start_with?(subcmd.to_s) }
+      keystroke.to_sym if subcmds.length > 1                                  # subcmd ready
+    end
+  end
+
+  def exit_mode_key
+    @config['exit_mode_key']
+  end
+
+  def subcmd_timeout
+    @config['subcmd_timeout']
+  end
+
+  # returns the portion of the config under the named mode
+  def _mode(name)
+    @config['modes'][name]
   end
 end
---
+++ lib/config_test.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require_relative './config'
+
+class TestFancyBuff < Minitest::Test
+  def setup
+    @config = Config.parse(IO.read('config.yml'))
+  end
+
+  def test_mode_cmds
+    # direct, subcmd, and wildcards from 'home' mode
+    assert_equal 'autosave_enable', @config.cmd('home', nil, 'a')
+    assert_equal :b, @config.cmd('home', nil, 'b')
+    assert_equal 'ms_buff_adj', @config.cmd('home', :b, 'a')
+    assert_equal :t, @config.cmd('home', nil, 't')
+    assert_equal 'mark_set a', @config.cmd('home', :m, 'a')
+    assert_equal 'mark_set b', @config.cmd('home', :m, 'b')
+    assert_equal 'mark_set c', @config.cmd('home', :m, 'c')
+
+    # confirming works in other modes as well
+    assert_equal 'pan_left', @config.cmd('pan', nil, 'h')
+    assert_equal :c, @config.cmd('select', nil, 'c')
+    assert_equal 'copy_to c', @config.cmd('select', :c, 'c')
+  end
+
+  def test_exit_mode_key_found
+    assert_equal :esc, @config.exit_mode_key
+  end
+
+  def test_subcmd_timeout
+    assert_equal 1, @config.subcmd_timeout
+  end
+
+  def test__mode
+    assert @config._mode('home').is_a?(Hash)
+    assert_equal 'autosave_enable', @config._mode('home')['a']
+  end
+end
