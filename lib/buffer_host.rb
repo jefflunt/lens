ff206b8b429dd96931741a00c80f86007923406c
jefflunt <jefflunt@gmail.com>
2025-03-28 22:33:01 -0500

    load buffer and wrap everything in a rescue

--- lib/buffer_host.rb
+++ lib/buffer_host.rb
@@ -1,14 +1,25 @@
 require 'socket'
+require 'rouge'
+require_relative './buffer'
 
-host, port = [ARGV.shift, ARGV.shift]
+begin
+  host, port = [ARGV.shift, ARGV.shift]
+  path = ARGV.shift
 
-puts "connecting to #{host}:#{port}"
-socket = TCPSocket.new(host, port)
+  puts "connecting to #{host}:#{port}"
+  socket = TCPSocket.new(host, port)
 
-loop do
-  req = socket.gets.chomp
-  puts "REQ: #{req}"
-  socket.puts(":ok")
+  buffer = Buffer.new(Rouge::Formatters::Terminal256.new, Rouge::Lexer.guess_by_filename(path))
+  IO
+    .readlines(filename)
+    .each{|l| buff << l.chomp }
+
+  loop do
+    req = socket.gets.chomp
+    puts "REQ: #{req}"
+    socket.puts(":ok")
+  end
 rescue NoMethodError, IOError
   socket&.close
+  break
 end
